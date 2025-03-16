42453b257011a66904a41566f2889d9652a7a73a
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-03-16 17:20:00 -0300

    Se agrega leyenda de Loading en componente Spinner

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-
 export const Spinner = () => {
   return (
-    <div className='flex justify-center items-center h-screen'>
+    <div className='flex flex-col justify-center items-center h-screen gap-4'>
+      <p className='text-[16px] font-semibold leading-5'>Loading...</p>
       <div className='w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin'></div>
     </div>
   );
