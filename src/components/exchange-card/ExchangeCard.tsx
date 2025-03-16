bb80df6293140bf380b1991be53d13eaa8692802
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-03-16 00:10:33 -0300

    Se aplican estilos para vista desktop del componente ExchangeCard

--- src/components/exchange-card/ExchangeCard.tsx
+++ src/components/exchange-card/ExchangeCard.tsx
@@ -3,8 +3,9 @@ import exchange from '../assets/icons/Button.svg';
 export const ExchangeCard = () => {
   return (
     <>
-      <div className='bg-white rounded-lg shadow-lg pt-8 pb-2 w-[311px] lg:w-[1126px]'>
-        <div className='flex flex-col lg:flex-row justify-center items-center gap-6'>
+      <div className='bg-white rounded-lg shadow-lg pt-8 pb-2 w-[311px] h-full lg:w-[1126px] lg:h-[402px]'>
+        {/* Exchange inputs */}
+        <div className='flex flex-col lg:flex-row justify-center lg:justify-between lg:px-10 items-center gap-6'>
           {/* Amount */}
           <div>
             <label className='block text-[16px] font-semibold mb-2'>Amount</label>
@@ -12,12 +13,12 @@ export const ExchangeCard = () => {
               <span className='absolute inset-y-0 left-0 pl-3 flex items-center'>$</span>
               <input
                 type='number'
-                className='w-[279px] lg:w-[276px] px-4 py-2 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-1 focus:ring-[#1A8DFF] text-[16px] font-semibold leading-5'
+                className='w-[279px] px-4 py-2 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-1 focus:ring-[#1A8DFF] text-[16px] font-semibold leading-5'
               />
             </div>
           </div>
 
-          <div className='flex flex-col gap-6 lg:flex-row'>
+          <div className='flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-evenly lg:w-full'>
             <div>
               <label className='block text-[16px] font-semibold mb-2'>From</label>
               <select className='w-[276px] px-4 py-2 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-1 focus:ring-[#1A8DFF] text-[16px] font-semibold leading-5'>
@@ -41,24 +42,24 @@ export const ExchangeCard = () => {
         </div>
 
         {/* Exchange Rate Display */}
-        <div className='mt-6 px-4'>
+        <div className='mt-6 px-4 lg:px-10 lg:mt-[70px]'>
           <div>
-            <div className='text-[24px] font-semibold leading-8'>
+            <div className='text-[24px] font-semibold leading-8 lg:text-[32px]'>
               1.00 Euro = <br />
               1.0627478 US Dollars
             </div>
             <div className='text-[16px] text-[#757575] font-normal leading-8 mt-1'>1 USD = 0.941004 EUR</div>
           </div>
 
-          <div className='flex flex-col justify-end items-end'>
-            <div className='hidden bg-[#E8F3FF] p-4 rounded-[8px] w-[518px] font-normal'>
-              <p className='text-[14px] text-black leading-8'>
+          <div className='hidden lg:flex flex-col justify-end items-end -mt-8'>
+            <div className=' bg-[#E8F3FF] px-6 py-5 rounded-[8px] w-[518px] font-normal shadow-[0_2px_4px_0_rgba(0,0,0,0.20)]'>
+              <p className='text-[14px] font-normal text-black leading-8'>
                 We use the mid-market rate for our Converter. This is for informational purposes only. You won’t receive
                 this rate when sending money.
               </p>
             </div>
 
-            <div className='hidden'>
+            <div className='hidden lg:block lg:mt-3'>
               <p className='text-[12px] font-light leading-8'>
                 Euro to US Dollar conversion — Last updated Dec 15, 2022, 19:17 UTC
               </p>
@@ -67,7 +68,7 @@ export const ExchangeCard = () => {
         </div>
       </div>
 
-      <div className='mt-4 px-10'>
+      <div className='mt-4 px-10 lg:hidden'>
         <p className='text-[12px] font-light leading-6'>
           <a
             href='https://www.xe.com/currency/eur-euro/'
