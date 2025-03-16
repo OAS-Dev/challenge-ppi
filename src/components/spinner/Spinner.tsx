7db50225ec75988a876d5a7887dc4e0ff0fc2d75
Oscar Ariel Saucedo <arielsaucedo.dev@gmail.com>
2025-03-16 02:13:49 -0300

    Se crea Spinner para loading

---
+++ src/components/spinner/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Spinner = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='w-16 h-16 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin'></div>
+    </div>
+  );
+};
